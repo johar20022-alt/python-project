c7c2550cae61caeba9c70cc9dc086de547d25e2e
mahmoud <johar20022@hotmail.com>
2026-02-15 02:38:00 +0300

    first python project

---
+++ welcome.py
@@ -0,0 +1,7 @@
+print("welcome to my python program!")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("\nHello",name)
+print("\nYou are ",age, "years old")
+print("Nice to meet you ")
